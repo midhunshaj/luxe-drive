a419cd7d894690b9e05e2e28417d57d5d316f159
Midhun Shaj <midhunshaj47@gmail.com>
2026-07-12 21:34:02 +0530

    fix: shebang in server-setup.sh

--- server-setup.sh
+++ server-setup.sh
@@ -1,4 +1,4 @@
-#!/bin/bash midhunshaj
+#!/bin/bash
 # ============================================================
 #  LuxeDrive - One-Time Server Setup Script
 #  Run this ONCE on your AWS server to prepare it for CI/CD
